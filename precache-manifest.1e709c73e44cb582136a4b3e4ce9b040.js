5abb4724475637cfd7c4ba99936b34980467a6d1
Symon Hasan <isymonhs@gmail.com>
2020-05-15 23:13:51 +0600

    Updates

---
+++ precache-manifest.1e709c73e44cb582136a4b3e4ce9b040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bdd814578b365a2571260eeb36a089d",
+    "url": "/cryptgen/index.html"
+  },
+  {
+    "revision": "7f5e74838ac9e14b546c",
+    "url": "/cryptgen/static/css/main.5e1b3d26.chunk.css"
+  },
+  {
+    "revision": "cf64a1e2cfa089aa20d8",
+    "url": "/cryptgen/static/js/2.4953589c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cryptgen/static/js/2.4953589c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f5e74838ac9e14b546c",
+    "url": "/cryptgen/static/js/main.2ea0dc69.chunk.js"
+  },
+  {
+    "revision": "7e4d44e09543d582b633",
+    "url": "/cryptgen/static/js/runtime-main.517ab807.js"
+  }
+]);
